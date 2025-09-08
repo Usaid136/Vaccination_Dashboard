814692329d187e8fd5f3b304879dc3e7a4bf674e
M.Usaid Saddiq <168702178+Usaid136@users.noreply.github.com>
2025-09-07 23:11:33 -0700

    Update vaccination.sql

--- vaccination.sql
+++ vaccination.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 31, 2024 at 08:26 PM
+-- Generation Time: Sep 08, 2025 at 08:10 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `vaccination`
 --
+CREATE DATABASE IF NOT EXISTS `vaccination` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `vaccination`;
 
 -- --------------------------------------------------------
 
